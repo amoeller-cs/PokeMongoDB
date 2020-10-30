ea6eaf817ea3a19b010e47ff237403161ca7d6b8
Ely Lam <eydelamb14@gmail.com>
2020-10-30 16:57:51 -0700

    Images loading on react app

--- front/src/App.js
+++ front/src/App.js
@@ -4,12 +4,42 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>PokeMongoDB</h1>
+    <div className="App" class="container">
+      <h1><img src="./images/pika.png" 
+          alt="Pikachu" 
+          title="Pikachu"
+          width="80"/>PokeMongoDB</h1>
 
       <p>Build Your Best Team!</p>
+
+      <form>
+        <label for="username">Username:</label><br/>
+        <input type="text" id="username"/>
+        <input type="submit" value="Submit" id="userSub"/>
+      </form>
+
+      <ul id="player"></ul>
+
+      <form>
+        <label for="search">Search for a pokemon to add to your team:</label>
+        <br/>
+        <input type="text" id="searchPokemon"/>
+        <input type="submit" value="Search" id="pokeDBSearch"/>
+      </form>
+
+      <br/>
+      <ul id="pokemon"></ul>
+
+      <footer>Created by Alex Moeller and Ely Lam 2020 <img src="./images/pokeball-favicon.png" 
+        alt="Pokeball" 
+        title="Pokeball"
+        width="30"/>
+      </footer>
     </div>
   );
 }
+
+// Image from https://www.freeiconspng.com/img/45343
+
 
 export default App;
