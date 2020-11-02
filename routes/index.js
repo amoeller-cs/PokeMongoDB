9ccf52ce95eac5b5e77b8937e0266004a1e86a9b
Alex Moeller <amoeller.psy@gmail.com>
2020-11-02 03:09:56 -0800

    cleaned code

--- routes/index.js
+++ routes/index.js
@@ -1,44 +1,36 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 const myDB = require("../db/myPokeMongoDB.js"); //pokedb
 
-/* GET home page. */
-router.get('/', (req, res, next) => {
-  res.send({ title: 'PokeMongoDB Create a Team!' });
-});
-
-router.get('/player', async (req, res, next) => {
+router.get("/player", async (req, res) => {
   const player = await myDB.getPlayer("alex");
-  res.json(player); // get player db; default "alex"
+  res.json(player); // get player db
 });
 
-router.get('/pokemon', async (req, res, next) => {
+router.get("/pokemon", async (req, res) => {
   const pokemon = await myDB.getPokemon();
   res.json(pokemon); // get pokemon db
 });
 
-router.post('/updateTeam', async (req, res, next) => {
+router.post("/updateTeam", async (req, res) => {
   let dex = req.body.position;
   dex = dex - 1;
   let pokemon = req.body.newPokemon;
-  let name = req.body.name;
   let user = req.body.user;
   myDB.setPokemon(user, "team", dex, pokemon);
-  const player = await myDB.getPlayer();
   res.redirect("/"); // redirect to home page
 });
 
-router.get('/start', async (req, res, next) => {
+router.get("/start", async (req, res) => {
   myDB.loadPokemon();
-  const pokemon = await myDB.getPokemon("Squirtle");
+  const pokemon = await myDB.getPokemon();
   res.json(pokemon);
 });
 
-router.post('/newUser', async (req, res, next) => {
+router.post("/newUser", async (req, res) => {
   let user = req.body.newUsername;
   myDB.createTeam(user);
-  const player = await myDB.getPlayer(user);
   res.redirect("/"); // redirect to home page
 });
 
