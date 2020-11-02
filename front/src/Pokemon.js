27cba05773c553b1cebafa31a5a81abae8d99d57
Alex Moeller <amoeller.psy@gmail.com>
2020-11-02 15:17:43 -0800

    added style

--- front/src/Pokemon.js
+++ front/src/Pokemon.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
+import "./styles/Pokemon.css";
 
 function Pokemon(props) {
   const [search, setPokemon] = useState("");
