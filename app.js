39458c02ce4b86870b965b506f1489c4a4de0df1
Alex Moeller <amoeller.psy@gmail.com>
2020-11-02 17:59:37 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,24 +1,18 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-const indexRouter = require('./routes/index');
+const indexRouter = require("./routes/index");
 
 const app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'front/build')));
+app.use(express.static(path.join(__dirname, "front/build")));
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
-app.listen(9000);
-
-// app.use('/', indexRouter);
+app.use("/", indexRouter);
 
 module.exports = app;
